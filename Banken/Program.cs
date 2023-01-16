41bc3561f2d6c38ddf3281dd7aa2e4f24986b0ae
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-01-16 17:23:18 +0100

    Added log-in system

--- Banken/Program.cs
+++ Banken/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace file_io;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*string input = "";
+        try
+        {
+            input = File.ReadAllText(@"accounts.txt");
+        }
+        catch
+        {
+            Console.WriteLine("No file found...");
+        }*/
+
+        Console.WriteLine("Välkommen till banken!");
+        bool mainMenu = true;
+        while (mainMenu)
+        {
+            Console.WriteLine("Var vänlig och ange följande användaruppgifter: ");
+            Console.WriteLine("Användarnamn: ");
+            Console.ReadLine();
+            Console.WriteLine("Pin-kod: ");
+            Console.ReadLine();
+            string? choice = Console.ReadLine().ToUpper();
+            switch (choice)
+            {
+                case "1":
+                    break;
+            }
+        }
+    }
+}
